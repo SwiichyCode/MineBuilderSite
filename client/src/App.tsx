736675ea77ddb1d7d7ab6f7578845daa39e5cbef
Swiichy <adlpromail@gmail.com>
2023-05-26 17:36:17 +0200

    feat: implement styled-components settings / add path aliases

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import { ThemeProvider } from "styled-components";
+import { theme } from "@/styles/theme";
+import GlobalStyle from "@/styles/globalStyles";
+
+export const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        <GlobalStyle></GlobalStyle>
+      </Fragment>
+    </ThemeProvider>
+  );
+};
