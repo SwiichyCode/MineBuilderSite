e14911afd85959a5183bc2df17d9cf5881d4607d
Swiichy <adlpromail@gmail.com>
2023-06-06 02:59:48 +0200

    .

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,20 +2,12 @@ import { Fragment } from "react";
 import { ThemeProvider } from "styled-components";
 import { theme } from "@/styles/theme";
 import GlobalStyle from "@/styles/globalStyles";
-import AnimalExample from "@/components/AnimalExemple";
-import AnimalExampleWithApiStates from "@/components/AnimalExempleWithApiStates";
-import AnimalExampleWithUseApiStatus from "./components/AnimalExampleWithUseApiStatus";
-import AnimalExampleWithUseApi from "./components/AnimalExempleWithUseApi";
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Fragment>
         <GlobalStyle />
-        {/* <AnimalExample /> */}
-        {/* <AnimalExampleWithApiStates /> */}
-        {/* <AnimalExampleWithUseApiStatus /> */}
-        <AnimalExampleWithUseApi />
       </Fragment>
     </ThemeProvider>
   );
